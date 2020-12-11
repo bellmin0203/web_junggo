a05f81f56ff32edae37de17b59df88b3f121ea86
hello-hama <38874662+hello-hama@users.noreply.github.com>
2020-12-11 21:53:32 +0900

    board 미들웨어 notice 미들웨어로 변경 등
    미들웨어 이름 변경 (board => notice)
    공지사항 관리자만 글쓰기 가능
    JWT auth 미들웨어로 구현

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 // ./app.js
 
-const dotenv = require('dotenv');
-const path = require('path');
+const dotenv = require("dotenv");
+const path = require("path");
 const PORT = 80;
-dotenv.config({ path: path.join(process.cwd(), 'config', '.env.development') });
+dotenv.config({ path: path.join(process.cwd(), "config", ".env.development") });
 
-const db = require('./lib/db');
+const db = require("./lib/db");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const methodOverride = require('method-override');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const methodOverride = require("method-override");
 
 const app = express();
-app.disable('view cache');
+app.disable("view cache");
 // router
-const router = require('./router');
+const router = require("./router");
 
 //bodyParser
-app.use(bodyParser.json())
-app.use(express.urlencoded( {extended : false } ));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // method-override
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 // 뷰엔진 설정
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
-app.use('/', (req,res,next)=>{
-    console.log("HTTP "+req.method+" "+req.path);
+app.use("/", (req, res, next) => {
+    console.log("HTTP " + req.method + " " + req.path);
     next();
 });
-app.use('/', router);
+app.use("/", router);
 
-app.listen(PORT, () => console.log("app listening at http://localhost:",PORT));
+app.listen(PORT, () => console.log("app listening at http://localhost:", PORT));
---
+++ router/middleware/notice.js
@@ -0,0 +1,104 @@
+const db = require("../../lib/db");
+const jwt = require("../../lib/jwt");
+
+module.exports.writeMiddleware = function (req, res, next) {
+    try {
+        const writer = decoded[res.locals.userNo];
+        const title = req.body.title;
+        const content = req.body.content;
+        var sql = "INSERT INTO NOTICE (writer, title, content) VALUES(?, ?, ?);";
+        const rows = db.query(sql, [writer, title, content]);
+        next();
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports.listMiddleware = (req, res, next) => {
+    try {
+        const accessToken = req.cookies.token;
+        const decoded = jwt.checkToken(accessToken, "access_token");
+        var userGrade = 1;
+
+        if(decoded)
+            userGrade = decoded['grade'];
+
+        var sql = "SELECT no, writer, title, date_format(dtCreate, '%Y.%m.%d %H:%i') `dtCreate` FROM NOTICE ORDER BY dtCreate DESC;";
+
+        const rows = db.query(sql);
+        var tempRows;
+        const writerRows = [];
+
+        rows.forEach(function (item) {
+            sql = "SELECT nickname FROM USER WHERE no = ?;";
+            tempRows = db.query(sql, [item.writer]);
+            writerRows.push(tempRows[0].nickname);
+        });
+
+        res.render("noticeList", {
+            title: "공지사항 - 평화나라",
+            page: "noticeList",
+            rows: rows,
+            writerRows: writerRows,
+            userGrade: userGrade
+        });
+        next();
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports.detailMiddleware = (req, res, next) => {
+    try {
+        const access_token = req.cookies.token;
+        const decoded = jwt.checkToken(access_token, "access_token");
+
+        if (decoded) {
+            const sql = "SELECT * FROM NOTICE WHERE no = ?";
+            const rows = db.query(sql, [req.params.no]);
+            console.log("NOTICE Read No => " + rows[0].no);
+            if (rows[0].writer == decoded["no"] && rows.length == 1) {
+                res.render("noticeDetail", {
+                    title: rows[0].title + " - 공지사항 - 평화나라",
+                    page: "noticeDetail",
+                    rows: rows,
+                    writer: true,
+                    token: req.cookies.token,
+                });
+            } else if (rows.length == 1) {
+                res.render("noticeDetail", {
+                    title: rows[0].title + " - 공지사항 - 평화나라",
+                    page: "noticeDetail",
+                    rows: rows,
+                    writer: false,
+                    token: req.cookies.token,
+                });
+            }
+        } else {
+            res.render("noticeDetail", {
+                title: rows[0].title + " - 공지사항 - 평화나라",
+                page: "noticeDetail",
+                rows: rows,
+                writer: false,
+                token: req.cookies.token,
+            });
+        }
+    } catch (err) {
+        res.status(401).send({ error: "token expired" });
+    }
+};
+
+module.exports.deleteMiddleware = (req, res, next) => {
+    try {
+        var sql = "SELECT writer FROM NOTICE WHERE no = ?";
+        const writer = db.query(sql, [req.params.no]);
+
+        if (writer == decoded[res.locals.userNo]) {
+            sql = "DELETE FROM NOTICE WHERE no = ?";
+            db.query(sql, [req.params.no]);
+            next();
+        }
+    } catch (err) {
+        next(err);
+    }
+};
