c80359d5c220cf0cb9d9e15e563613917b296dc8
hello-hama <38874662+hello-hama@users.noreply.github.com>
2020-12-18 23:30:15 +0900

    인기검색어 구현
    SEARCH_HISTORY에 5개 미만의 데이터가 있을 시는 에러 발생..

--- router/middleware/goods.js
+++ router/middleware/goods.js
@@ -67,3 +67,66 @@ module.exports.recentlyGoods = (req, res, next) => {
     res.locals.goods = recGoods;
     next();
 }
+
+/* GET '/' => 인기 검색어 */
+module.exports.searchTop = (req, res, next) => {
+    const searchHis = db.query("SELECT word FROM SEARCH_HISTORY ORDER BY count DESC, word LIMIT 5;");
+    
+    if(searchHis.length != 0){
+        res.locals.searchHis = searchHis
+        
+    }
+    console.log(searchHis);
+    next();
+}
+
+/* GET '/search' => 검색 이벤트 */
+module.exports.search = (req, res, next) => {
+    const word = req.query.search_word;
+    const page = req.params.page===undefined?1:parseInt(req.params.page);
+
+    const goods = db.query("\
+    SELECT b.`no`, u.nickname `writer`, title, content, price, bs.strName `status`, c.strName `category`, ct.strName `city`, photo FROM BOARD b\
+	LEFT JOIN `USER`u ON u.`no` = b.writer\
+	LEFT JOIN BOARD_STATUS bs ON b.status = bs.id\
+	LEFT JOIN CITY_TYPE ct ON b.city = ct.id\
+    LEFT JOIN CATEGORY c ON b.category = c.id\
+    LIMIT ?, ?;\
+    ",[(page-1)*ONE_PAGE_CONTENT_COUNT, ONE_PAGE_CONTENT_COUNT]);
+    const rows = db.query("SELECT count(*) 'count' FROM BOARD")
+    const maxPage = Math.ceil((rows[0].count)/ONE_PAGE_CONTENT_COUNT)
+    const nextPages = [];
+    const prevPages = [];
+    const prevLimit = (maxPage - page)>3?3:(maxPage - page);
+    for(var i=page-1;i>0;i--){
+        prevPages.push(i);
+        if(prevPages.length>=(6-prevLimit)) break;
+    }
+    
+    for(var i=page+1;i<=maxPage;i++){
+        nextPages.push(i);
+        if(nextPages.length>=6-prevPages.length) break;
+    }
+
+    const pageInfo = {
+        maxPage : maxPage,
+        nowPage : page,
+        nextPages : nextPages,
+        prevPages : prevPages.reverse(),
+        onePageContent : ONE_PAGE_CONTENT_COUNT,
+    }
+    console.log(pageInfo);
+    res.locals.goods = goods;
+    res.locals.pageInfo = pageInfo;
+
+    const search = db.query("SELECT * FROM SEARCH_HISTORY WHERE word = ?", [word]);
+    if(search.length == 1){
+        var count = parseInt(search[0].count)+1;
+        console.log("검색 : "+word);
+        db.query("UPDATE SEARCH_HISTORY SET count = ? WHERE id = ?", [count, search[0].id]);
+    } else{
+        console.log("검색 : "+word);
+        db.query("INSERT INTO SEARCH_HISTORY(word) VALUES(?)", [word]);
+    }
+    next();
+}
